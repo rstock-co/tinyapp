bd5fa69a0e6d42f16c7cd380daa291f3557f3d36
Richard Stock <rstock.co@gmail.com>
2022-09-05 22:27:22 -0600

    Edit random string generation function

--- express_server.js
+++ express_server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-const generateRandomString = (numChars, stringLength) => Math.random().toString(numChars).substring(0,stringLength);
-
+const generateRandomString = (numChars, stringLength) => Math.random().toString(numChars).substring(3,stringLength + 3);
 
 app.set("view engine", "ejs");
 
