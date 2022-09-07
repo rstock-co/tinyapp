4faf78813111669621c6d0eb488a9135074fcb3c
Richard Stock <rstock.co@gmail.com>
2022-09-06 21:14:43 -0600

    Edit header.ejs to display username instead of login form if user is logged in

--- express_server.js
+++ express_server.js
@@ -55,7 +55,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
