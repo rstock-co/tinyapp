c725cba621ed09ce11e2753679f9f2ddbeff591f
Richard Stock <rstock.co@gmail.com>
2022-09-07 16:22:54 -0600

    Add GET and POST endpoints for registration page and update HTML for registration form

--- express_server.js
+++ express_server.js
@@ -105,6 +105,17 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_register",templateVars);
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(email,password)
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
