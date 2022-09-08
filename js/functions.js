ea912991f4216754772385c989a3859d786e31de
Richard Stock <rstock.co@gmail.com>
2022-09-07 23:50:49 -0600

    Add helper functions for error handling for DRY

--- js/functions.js
+++ js/functions.js
@@ -24,4 +24,29 @@ const urlsForUser = (id, urlDatabase) => {
   return urls;
 }
 
-module.exports = { generateID, getUserByEmail, urlsForUser };
+const errNotLoggedIn = (res, cookie) => {
+  if (!cookie) {
+    res.send("You must register or be logged in to do this.");
+    return true;
+  }
+  return false;
+}
+
+const errDoesNotExist = (res, id, database) => {
+  // if URL doesn't exist
+  if (!database[id]) {
+    res.send("This URL does not exist. Click on 'Create New URL' to add new URLs.");
+    return true;
+  }
+  return false;
+}
+const errDoesNotBelongToUser = (res, id, cookie, database) => {
+  // if URL doesn't belong to user
+  if (database[id].userID !== cookie) {
+    res.send("This URL is not in your database. Click on 'Create New URL' to add new URLs.");
+    return true;
+  }
+  return false;
+}
+
+module.exports = { generateID, getUserByEmail, urlsForUser, errNotLoggedIn, errDoesNotExist, errDoesNotBelongToUser };
