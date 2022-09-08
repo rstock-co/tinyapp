a0f1f11d5730ab7a68b294ccaa4921e8996adf53
Richard Stock <rstock.co@gmail.com>
2022-09-08 16:58:20 -0600

    add two Mocha tests for getUserByEmail function

---
+++ test/helpersTest.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../js/helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.strictEqual(user.id, expectedUserID);
+    assert.deepEqual(user,{
+        id: "userRandomID", 
+        email: "user@example.com", 
+        password: "purple-monkey-dinosaur"
+      });
+  });
+  it("should return a `null` object when sending a non-existent email", () => {
+    const user = getUserByEmail("", testUsers);
+    const expectedUserID = undefined;
+    assert.strictEqual(user, null);
+    
+  });
+});
