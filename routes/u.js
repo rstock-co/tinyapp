251d72fa7a295e089073ddcbe9323e5c49823d28
Richard Stock <rstock.co@gmail.com>
2022-09-10 12:48:17 -0600

    Improve comments for u.js

--- routes/u.js
+++ routes/u.js
@@ -3,21 +3,25 @@ const express = require("express");
 const router = express.Router();
 const { urlDatabase, users } = require("../db");
 
-// ---------- HELPER FUNCTIONS
-
 const { errDoesNotExist, handleErrors } = require("../helpers/errors");
 
-// SHORT TO LONG URL REDIRECT PAGE
+/**
+ *  Redirects to the longURL associated with the short URL :id
+ *  Displays error message if the given :id does not exist 
+ */
+
 router.get("/:id", (req, res) => {
   const userID = req.session.user_id;
   const id = req.params.id;
   const errors = handleErrors({
     exists: errDoesNotExist(id, urlDatabase),
   });
+
   if (errors === false) {
     const longURL = urlDatabase[id].longURL;
     return res.redirect(longURL);
   }
+
   const templateVars = { errors, users, id, userID };
   return res.render("error", templateVars);
 });
