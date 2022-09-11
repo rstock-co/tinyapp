85c457074a937e760a0cc36c96c4ef7963ca9f67
Richard Stock <rstock.co@gmail.com>
2022-09-10 20:04:17 -0600

    (Stretch) Add function to convert today's date to string

--- helpers/generic.js
+++ helpers/generic.js
@@ -33,4 +33,15 @@ const appendHttp = (url) => {
   return url;
 }
 
-module.exports = { generateID, getUserByEmail, urlsForUser, appendHttp };
+const convertTodayToString = () => {
+    var today = new Date();
+  
+    var dd = String(today.getDate());
+    var mm = String(today.getMonth() + 1);
+    var yyyy = today.getFullYear();
+  
+    return mm + "/" + dd + "/" + yyyy;
+  };
+  
+
+module.exports = { generateID, getUserByEmail, urlsForUser, appendHttp, convertTodayToString };
